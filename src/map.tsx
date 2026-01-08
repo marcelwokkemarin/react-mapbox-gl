8353f307f5569c1b752746b144a5e5fe0ad51b60
mwokke <m.wokke@marin.nl>
2026-01-08 17:27:47 +0100

    update package json and map.tsx

--- src/map.tsx
+++ src/map.tsx
@@ -190,7 +190,7 @@ const ReactMapboxFactory = ({
       } = this.props;
 
       // tslint:disable-next-line:no-any
-      //(MapboxGl as any).accessToken = accessToken;
+      // (MapboxGl as any).accessToken = accessToken;
       if (apiUrl) {
         // tslint:disable-next-line:no-any
         (MapboxGl as any).config.API_URL = apiUrl;
